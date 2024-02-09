dab1715465d4880f51b8a9c699b1588eb5f5a9af
Annateresa01 <shajianna013@gmail.com>
2024-02-09 10:48:50 +0530

    ok

--- controllers/signuprouter.js
+++ controllers/signuprouter.js
@@ -5,8 +5,8 @@ const router=express.Router()
 
 router.post("/user",async(req,res)=>{
     let data=req.body
-    //let users=new signrouter(data)
-   // let response=await users.save()
+    let users=new signrouter(data)
+   let response=await users.save()
     res.json(
         {
             status:"Success"
